66e34a5ad773229f1fe15024c40fdb8dfbae8f43
MehCheniti <mehdiwx91@hotmail.com>
2020-10-21 15:21:31 +0200

    Found a better solution to the Missing Number problem in CSES.

--- IN2010/CSES/MissingNumber.java
+++ IN2010/CSES/MissingNumber.java
@@ -4,25 +4,27 @@ public class MissingNumber{
 
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
-    //System.out.println("How many numbers?");
-    int i = scan.nextInt();
-    int[] arr = new int[i - 1];
-    //System.out.println("Input the numbers, between 1 and " + i +
-    //" (both inclusive).");
-    int arraySum = 0;
+    // System.out.println("How many numbers?");
+    long i = scan.nextInt();
+    // int[] arr = new int[i - 1];
+    // System.out.println("Input the numbers, between 1 and " + i +
+    // " (both inclusive).");
+    long arraySum = 0;
 
-    for (int x = 0; x < arr.length; x++){
-      int y = scan.nextInt();
-      if (y >= 1 && y <= i){
-        arr[x] = y;
-        arraySum += y;
-      } else{
-        x--;
-      }
+    for (int x = 0; x < i - 1; x++){
+      // int y = scan.nextInt();
+      // if (y >= 1 && y <= i){
+      //   arr[x] = y;
+      //   arraySum += y;
+      // } else{
+      //   x--;
+      // }
+      long y = scan.nextInt();
+      arraySum += y;
     }
 
-    int sum = i * (i + 1) / 2;
-    //System.out.println("The missing number is " + (sum - arraySum) + ".");
+    long sum = i * (i + 1) / 2;
+    // System.out.println("The missing number is " + (sum - arraySum) + ".");
     System.out.println((sum - arraySum));
   }
 
