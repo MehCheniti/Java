461fbede0eaa7dfd1a47e6df0b7bed1d11c87cb8
MehCheniti <mehdiwx91@hotmail.com>
2020-11-15 20:35:57 +0100

    Done some progress on the CSES repetitions program.

--- IN2010/CSES/Repetitions.java
+++ IN2010/CSES/Repetitions.java
@@ -6,20 +6,42 @@ public class Repetitions{
     Scanner scan = new Scanner(System.in);
     System.out.println("Please input the DNA sequence (in capital letters).");
     String str = scan.nextLine();
-    int counter = 0;
+    int max = 0;
+    int aCounter = 0;
+    int cCounter = 0;
+    int gCounter = 0;
+    int tCounter = 0;
 
     if (str.matches("[ACGT]+")){
       for (int i = 0; i < str.length(); i++){
-        for (int j = i + 1; j < str.length(); j++){
-          if (str.charAt(i) == str.charAt(j)){
-            counter++;
-          }
+        if (str.charAt(i) == 'A' && str.charAt(i) == str.charAt(i + 1)){
+          aCounter++;
+        } else if (str.charAt(i) == 'C' && str.charAt(i) == str.charAt(i + 1)){
+          cCounter++;
+        } else if (str.charAt(i) == 'G' && str.charAt(i) == str.charAt(i + 1)){
+          gCounter++;
+        } else if (str.charAt(i) == 'T' && str.charAt(i) == str.charAt(i + 1)){
+          tCounter++;
         }
       }
-      // System.out.println(counter);
     } else{
       System.out.println("Error compiling DNA sequence.");
     }
+
+    if (aCounter > cCounter && aCounter > gCounter && aCounter > tCounter){
+      max = aCounter;
+    } else if (cCounter > aCounter && cCounter > gCounter && cCounter >
+    gCounter){
+      max = cCounter;
+    } else if (gCounter > aCounter && gCounter > cCounter && gCounter >
+    tCounter){
+      max = gCounter;
+    } else if (tCounter > aCounter && tCounter > cCounter && tCounter >
+    gCounter){
+      max = tCounter;
+    }
+
+    System.out.println(max);
   }
 
 }
