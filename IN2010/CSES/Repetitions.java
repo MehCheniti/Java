62ad34114bb675b56a3c68dd72fcbde2021311db
MehCheniti <mehdiwx91@hotmail.com>
2020-11-16 23:04:19 +0100

    Done with the repetitions algorithm in CSES.

--- IN2010/CSES/Repetitions.java
+++ IN2010/CSES/Repetitions.java
@@ -4,7 +4,7 @@ public class Repetitions{
 
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
-    System.out.println("Please input the DNA sequence (in capital letters).");
+    // System.out.println("Please input the DNA sequence (in capital letters).");
     String str = scan.nextLine();
     int max = 0;
     int aCounter = 0;
@@ -14,14 +14,16 @@ public class Repetitions{
 
     if (str.matches("[ACGT]+")){
       for (int i = 0; i < str.length(); i++){
-        if (str.charAt(i) == 'A' && str.charAt(i) == str.charAt(i + 1)){
-          aCounter++;
-        } else if (str.charAt(i) == 'C' && str.charAt(i) == str.charAt(i + 1)){
-          cCounter++;
-        } else if (str.charAt(i) == 'G' && str.charAt(i) == str.charAt(i + 1)){
-          gCounter++;
-        } else if (str.charAt(i) == 'T' && str.charAt(i) == str.charAt(i + 1)){
-          tCounter++;
+        if (i + 1 < str.length()){
+          if (str.charAt(i) == 'A' && str.charAt(i) == str.charAt(i + 1)){
+            aCounter++;
+          } else if (str.charAt(i) == 'C' && str.charAt(i) == str.charAt(i + 1)){
+            cCounter++;
+          } else if (str.charAt(i) == 'G' && str.charAt(i) == str.charAt(i + 1)){
+            gCounter++;
+          } else if (str.charAt(i) == 'T' && str.charAt(i) == str.charAt(i + 1)){
+            tCounter++;
+          }
         }
       }
     } else{
@@ -41,7 +43,7 @@ public class Repetitions{
       max = tCounter;
     }
 
-    System.out.println(max);
+    System.out.println(max + 1);
   }
 
 }
